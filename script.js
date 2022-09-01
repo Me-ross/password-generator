b0cae60e577c9cef52e099ecb22bd4533223c4a4
Niloufar Sabbaghian <niloufarsabbaghian@Niloufars-iMac.local>
2022-08-31 22:50:29 -0700

    updated code to check for character type selection

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-
-// function writePassword() {
-//   var finalPassword = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = finalPassword;
-// }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
 
-var chosenLength = '';
-
+//Prompt for the length desired
 function generatePassword() {
- let chosenLength = window.prompt('How many characters would you like your password to contain? Choose a length between 8-128 characters');
- console.log('user initial chosen length is ' + chosenLength);
- checkLength();
+  chosenLength = window.prompt('How many characters would you like your password to contain? Choose a length between 8-128 characters');
+  console.log('user initial chosen length is ' + chosenLength);
+  checkLength();
 }
 
 // check length parameters
 function checkLength() {
-  if (chosenLength === null) {
+  if (chosenLength == null) {
     return;
   } 
 
@@ -47,7 +38,7 @@ var UpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 function characterType() {
   checkSpecial();
   function checkSpecial() {
-    var useSpecialCharacters = window.confirm('Click OK to confirm using Special Characters');
+    useSpecialCharacters = window.confirm('Click OK to confirm using Special Characters');
     console.log('special char ' + useSpecialCharacters);
     if (!useSpecialCharacters){
       specialCharacters.length = 0;
@@ -60,7 +51,7 @@ function characterType() {
   }
 
   function checkNumeric() {
-    var useNumeric = window.confirm('Click OK to confirm using Numberic Characters');
+    useNumeric = window.confirm('Click OK to confirm using Numberic Characters');
     console.log ('numeric ' + useNumeric);
     if (!useNumeric) {
       numeric.length = 0;
@@ -73,7 +64,7 @@ function characterType() {
   }
 
   function checkLower() {
-    var useLowerCase = window.confirm('Click OK to confirm using Lower Case Characters');
+    useLowerCase = window.confirm('Click OK to confirm using Lower Case Characters');
     console.log ('lowercase ' + useLowerCase);
     if (!useLowerCase) {
       lowerCase.length = 0;
@@ -86,39 +77,51 @@ function characterType() {
   }
 
   function checkUpper() {
-    var useUpperCase = window.confirm('Click OK to confirm using Upper Case Characters');
+    useUpperCase = window.confirm('Click OK to confirm using Upper Case Characters');
     console.log ('uppercase ' + useUpperCase);
     if (!useUpperCase) {
       UpperCase.length = 0;
       console.log('length ' + UpperCase.length);
-      redoCharatertype();
+      checkCharatertype();
     } else {
       console.log('runnig true upper');
+      assignPassword();
     }
   }
 
     // At least one "type" of character required
-  function redoCharatertype(){
-    console.log ('arrived at redocharacter');
-    if (specialCharacters.length == 0 && numeric.length == 0 && lowerCase.length == 0 && UpperCase.length == 0) {
+  function checkCharatertype(){
+    console.log ('arrived at checkcharactertype');
+    if ((!useSpecialCharacters) && (!useNumeric) && (!useLowerCase) && (!useUpperCase)) {
+    // if (specialCharacters.length == 0 && numeric.length == 0 && lowerCase.length == 0 && UpperCase.length == 0) {
     window.alert ('Select at least One Special, Numeric, Lower case or Upper case character');
     characterType();
-    } else {
-      console.log('special char = ' + specialCharacters + ', numbers = ' + numeric + ', lowercase = ' + lowerCase + ', uppercase = ' + UpperCase);
+    } 
+    else {
+      console.log('arrived at checkchar is ok go to assign password');
+      assignPassword();
     }
   } 
+
+  function assignPassword(){
+    // total possible value of password and it's length
+    passwordArray = specialCharacters.concat(numeric, lowerCase, UpperCase);
+    console.log(passwordArray); 
+
+    // Based on the chosen length generate final password.
+    finalPassword = '';
+    console.log('chosen length = '+ chosenLength);
+    for(var i = 0; i < chosenLength; i++) {
+    var index = Math.floor(Math.random() * passwordArray.length);
+    finalPassword += passwordArray[index];
+    console.log(finalPassword);
+    writePassword();
+    }
+  }
+  // Write password to the #password input
+  function writePassword() {
+  passwordText = document.querySelector("#password");
+  passwordText.value = finalPassword + 'above text includes:';
+  }
 }
 
-console.log('special char = ' + specialCharacters + ', numbers = ' + numeric + ', lowercase = ' + lowerCase + ', uppercase = ' + UpperCase);
-// total possible value of password and it's length
-var passwordArray = specialCharacters.concat(numeric, lowerCase, UpperCase);
-console.log(passwordArray);
-
-
-// Based on the chosen length generate final password.
-var finalPassword = '';
-for(var i = 0; i < chosenLength; i++ ) {
-var index = Math.floor(Math.random() * passwordArray.length);
-finalPassword += passwordArray[index];
-}
-console.log(finalPassword);
